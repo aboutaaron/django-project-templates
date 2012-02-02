d49a1144b3b0f7f9594a3358711fdf2c97f32461
Chase Davis <chase.davis@gmail.com>
2012-02-02 10:26:14 -0800

    Tiny fabfile change

---
+++ src/django_project_templates/templates/cir_project/+project+/configs/common/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.defaults import *
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/(.*)', admin.site.root),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT
+        }),
+   )
