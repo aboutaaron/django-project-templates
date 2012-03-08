f0281ee14fd623e54d2ad2bdb065e11107cd5b5c
Chase Davis <chase.davis@gmail.com>
2012-03-07 17:07:24 -0800

    Changed static URLs

--- src/django_project_templates/templates/heroku_project/+project+/urls.py
+++ src/django_project_templates/templates/heroku_project/+project+/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
@@ -13,9 +14,4 @@ urlpatterns = patterns('',
 
 )
 
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.STATIC_ROOT
-        }),
-   )
+urlpatterns += staticfiles_urlpatterns()
